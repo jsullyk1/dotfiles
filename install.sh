5577d19205d01d486104b50742c464a7a269aae6
Jeremiah Sullenger <jeremiah@1x.tech>
2024-09-04 13:50:09 +0200

    Simplify

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
+# Theme Name
+ZSH_THEME="sully"
+# Path to the .zshrc file
+ZSHRC_FILE="$HOME/.zshrc"
+
+# ============================================================================================
+
 cd "$(dirname "$0")"
-cp ./dotfiles/.gitconfig ~
 
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+if [ ! -d "$DIRECTORY" ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+fi
 
-cp -R ./dotfiles/.oh-my-zsh ~
-cp ./dotfiles/.zshrc ~
+cp -R ./dotfiles/.oh-my-zsh $HOME
+
+# Replace oh-my-zsh theme.
+sed -i.bak "s/^ZSH_THEME=\"[^\"]*\"/ZSH_THEME=\"$NEW_THEME\"/" "$ZSHRC_FILE"
