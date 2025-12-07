fcf36bba60249b1c3c0ecbfc809090369e248ff9
Jeremiah Sullenger <jeremiah.sullenger@zivid.com>
2025-12-07 01:14:14 +0100

    Adding some vim plugins

---
+++ nvim/.config/nvim/lua/plugins/statusbar.lua
@@ -0,0 +1,22 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+            options = {
+                theme = 'onedark',
+                section_separators = '',
+                component_separators = '',
+                icons_enabled = true,
+            },
+            sections = {
+                lualine_a = { 'mode'},
+                lualine_b = { 'branch', 'diff', 'diagnostics'},
+                lualine_c = { 'filename' },
+                lualine_x = { 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
+            },
+        }
+    end,
+}
