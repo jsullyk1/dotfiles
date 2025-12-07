557da1439bd2f522945f2349148c2ef2f0f962b1
Jeremiah Sullenger <jeremiah.sullenger@zivid.com>
2025-12-07 02:11:51 +0100

    Fix status line for vim and tmux together add stow for tmux.

--- nvim/.config/nvim/lua/plugins/statusbar.lua
+++ nvim/.config/nvim/lua/plugins/statusbar.lua
@@ -1,22 +1,35 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup {
-            options = {
-                theme = 'onedark',
-                section_separators = '',
-                component_separators = '',
-                icons_enabled = true,
-            },
-            sections = {
-                lualine_a = { 'mode'},
-                lualine_b = { 'branch', 'diff', 'diagnostics'},
-                lualine_c = { 'filename' },
-                lualine_x = { 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' },
-            },
-        }
-    end,
+    { 'vimpostor/vim-tpipeline',
+        config = function()
+            -- Embedd into tmux
+            vim.g.tpipeline_autoembed = 1
+        end,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        event= "VeryLazy",
+        config = function()
+            require('lualine').setup({
+                options = {
+                    theme = 'onedark',
+                    section_separators = '',
+                    component_separators = '',
+                    icons_enabled = true,
+                },
+                sections = {
+                    lualine_a = { 'mode'},
+                    lualine_b = { 'branch', 'diff', 'diagnostics'},
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' },
+                },
+            })
+
+            if os.getenv('TMUX') then
+                vim.defer_fn(function() vim.o.laststatus=0 end, 0)
+            end
+        end,
+    },
 }
--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
+source _stow.sh
 
 sudo apt update && sudo apt install -y \
     tmux
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+slide_and_stow "tmux" $HOME
