ba43ae3ba26936f6ddfd96e217aad70a6bd42a7d
Jeremiah Sullenger <jsully.k1@gmail.com>
2025-12-12 23:38:37 +0100

    Switching to zap for zsh plugins

--- zsh.sh
+++ zsh.sh
@@ -2,12 +2,15 @@
 set -e
 source _stow.sh
 
-sudo apt update
-sudo apt install -y zsh
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+if [ -x "$(command -v apt)" ]; then
+    sudo apt update
+    sudo apt install -y zsh
 fi
 
+if [ -x "$(command -v brew)" ]; then
+    brew install zsh
+fi
+
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
 slide_and_stow zsh $HOME
-slide_and_stow oh-my-zsh $HOME
