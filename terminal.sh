9e7ee24087e3ad9c0b092f1b6669ac44ce8ab761
Jeremiah Sullenger <jeremiah.sullenger@zivid.com>
2025-12-18 22:37:57 +0100

    Add some themes

--- terminal.sh
+++ terminal.sh
@@ -13,6 +13,20 @@ if [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
 fi
 
 if [ -x "$(command -v apt)" ]; then
+    sudo apt update
+    sudo apt install -y zsh
+fi
+
+if [ -x "$(command -v brew)" ]; then
+    brew install zsh
+fi
+
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
+slide_and_stow zsh $HOME
+
+
+if [ -x "$(command -v apt)" ]; then
     sudo apt update && sudo apt install -y \
         tmux
 fi
