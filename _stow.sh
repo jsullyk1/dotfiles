047bd1ca624930f33ab16935361246423e9b1507
Jeremiah Sullenger <jeremiah.sullenger@zivid.com>
2026-05-27 12:44:44 +0200

    Linting bash script

--- _stow.sh
+++ _stow.sh
@@ -1,16 +1,14 @@
-
 # Move any existing dotfiles out of the way then stow.
 function slide_and_stow() {
     package=$1
     target=$2
-    files=$(stow -n ${package} 2>&1 | grep "existing target" | awk -F': ' '{print $2}')
-    for file in $files; do
+    files=$(stow -n "$package" 2>&1 | grep "existing target" | awk -F': ' '{print $2}')
+    for file in "${files[@]}"; do
         backup=$target/dotfiles.bak/$file
-        install -d $(dirname $backup)
+        install -d "$(dirname "$backup")"
         echo "Sliding $file to $backup"
-        mv $target/$file $backup
+        mv "$target/$file" "$backup"
     done
 
-    stow --restow $package -t $target
+    stow --restow "$package" -t "$target"
 }
-
